1ebf4d4a800e1a1cafb764206d3d2cca72954e68
Rez21 <banprasad21@gmail.com>
2022-08-12 00:20:23 +0530

    [Raghav] Add: UC1 for Employee attendance

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+            Console.WriteLine("\nEmployee Attendance");
+            UC1_Employee_Attendance obj= new UC1_Employee_Attendance();
+            obj.CheckAttendance();
+        }
+    }
+}
