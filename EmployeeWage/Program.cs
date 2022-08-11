d9a0e75d5542810d8a577eafec16700341e60ab5
Rez21 <banprasad21@gmail.com>
2022-08-12 00:38:49 +0530

    [Raghav] Add: UC2- Employee daily wage calculation

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,9 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+            // UC1 - Employee attendance
             Console.WriteLine("\nEmployee Attendance");
             UC1_Employee_Attendance obj= new UC1_Employee_Attendance();
             obj.CheckAttendance();
+            // UC2- Employee Wage calculation
+            Console.WriteLine("Employee Wage Calculation");
+            UC2_EmployeeWage_Calculation wage = new UC2_EmployeeWage_Calculation();
+            wage.DailyWage();
         }
     }
 }
---
+++ EmployeeWage/UC2_EmployeeWage_Calculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class UC2_EmployeeWage_Calculation
+    {
+        public void DailyWage()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Full_Time)
+            {
+                int empRatePerHr = 20;
+                int empHour = 8;
+                int salary = (empRatePerHr * empHour);
+                Console.WriteLine("Employee's Full Day salary is: " + salary);
+            }
+            else
+            {
+                int salary = 0;
+                Console.WriteLine("Employee is Absent then salary is:" + salary);
+            }
+        }
+    }
+}
