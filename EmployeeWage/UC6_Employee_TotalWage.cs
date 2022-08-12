84228f2e0c69c602cd0ad39327d1259c4b55b70d
Rez21 <banprasad21@gmail.com>
2022-08-12 08:57:31 +0530

    [Raghav] Add: Total wage update

--- EmployeeWage/UC6_Employee_TotalWage.cs
+++ EmployeeWage/UC6_Employee_TotalWage.cs
@@ -8,5 +8,43 @@ namespace EmployeeWage
 {
     internal class UC6_Employee_TotalWage
     {
+        public void EmpWageTotal()
+        {
+            const int Part_Time = 2;
+            const int Full_Time = 1;
+            const int empRatePerHr = 20;
+            const int numOfWorkDay = 20;
+            const int MaxWorkHr = 100;
+
+            int empHr = 0;
+            int totalwage = 0;
+            int totalWorkDays = 0;
+            int totalEmpHrs = 0;
+
+            while (totalWorkDays < numOfWorkDay && totalEmpHrs <= MaxWorkHr)
+            {
+                totalWorkDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case Full_Time:
+                        empHr = 8;
+                        break;
+                    case Part_Time:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+
+                }
+                totalEmpHrs += empHr;
+            }
+            totalwage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Day: " + totalWorkDays + "\nEmployee working Hrs: " + totalEmpHrs);
+            Console.WriteLine("Total Salary is: " + totalwage);
+        }
     }
 }
