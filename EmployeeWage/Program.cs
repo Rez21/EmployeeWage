0bee15288cbaabde41a84a09e86724d6df14aeee
Prasad <banprasad21@gmail.com>
2022-08-17 06:50:20 +0530

    [Raghav] Add: Refactored Employee Wage code using class & methods

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,33 +2,49 @@
 {
     internal class Program
     {
+        public const int Part_Time = 2;
+        public const int Full_Time = 1;
+        public const int empRatePerHr = 20;
+        public const int numOfWorkDay = 20;
+        public const int MaxWorkHr = 100;
+
+        public static int computeEmpWage()
+        {
+            int empHr = 0;
+            int totalwage = 0;
+            int totalWorkDays = 0;
+            int totalEmpHrs = 0;
+
+            while(totalEmpHrs <= MaxWorkHr && totalWorkDays < numOfWorkDay)
+            {
+                totalWorkDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case Full_Time:
+                        empHr = 8;
+                        break;
+                    case Part_Time:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+
+                }
+                totalEmpHrs += empHr;
+            }
+            totalwage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Day: " + totalWorkDays + "\nEmployee working Hrs: " + totalEmpHrs);
+            Console.WriteLine("Total Salary is: " + totalwage);
+            return totalwage;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-            // UC1 - Employee attendance
-            Console.WriteLine("\nEmployee Attendance");
-            UC1_Employee_Attendance obj= new UC1_Employee_Attendance();
-            obj.CheckAttendance();
-            // UC2- Employee Wage calculation
-            Console.WriteLine("Employee Wage Calculation");
-            UC2_EmployeeWage_Calculation wage = new UC2_EmployeeWage_Calculation();
-            wage.DailyWage();
-            // UC3 - Part time wage calculation
-            Console.WriteLine("Employee Part-Time Wage Calculation");
-            UC3_PartTime_Wage partTime= new UC3_PartTime_Wage();
-            partTime.PartTimeWage();
-            // UC4 employee wage calculation using switch case
-            Console.WriteLine("Employee Wage Calculation using switch case");
-            UC4_EmployeeWage_using_switch wageCase = new UC4_EmployeeWage_using_switch();
-            wageCase.Empwage();
-            //UC 5 employee monthly wage calculation
-            UC5_MonthlyWage_Calculation monthlyWage = new UC5_MonthlyWage_Calculation();
-            monthlyWage.EmpWageMonth();
-            // UC6 Employee total wage calculation
-            UC6_Employee_TotalWage totalWage = new UC6_Employee_TotalWage();
-            totalWage.EmpWageTotal();
-                 
-
+            computeEmpWage();
         }
     }
 }
