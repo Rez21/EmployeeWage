61ff792f8cfeb3aba989a15bec88e6a5bf9d4e72
Prasad <banprasad21@gmail.com>
2022-08-22 01:08:11 +0530

    [Raghav] Add: total wage for companies using instance variable instead of functional paramaters

---
+++ EmployeeWage/EmpWageBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpWageBuilder
+    {
+        public const int Full_Time = 1;
+        public const int Part_Time = 2;
+
+        private string company;
+        private int empRatePerHr;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+
+        public EmpWageBuilder(string company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHr = empRatePerHr;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+
+
+        public void computeEmpWage()
+        {
+            int empHr = 0;
+            int totalWorkDays = 0;
+            int totalEmpHrs = 0;
+
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkDays < this.numOfWorkingDays)
+            {
+                totalWorkDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case Full_Time:
+                        empHr = 8;
+                        break;
+                    case Part_Time:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                totalEmpHrs += empHr;
+                Console.WriteLine("Day: " + totalWorkDays + " Emp Hrs: " +empHr);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHr;
+            
+            Console.WriteLine("Total Wage for company: " + company + " is: " + totalEmpWage);
+           
+        }
+
+        public string ToString()
+        {
+            return "Total Employee Wage for Company: " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,44 +2,14 @@
 {
     internal class Program
     {
-        public const int Part_Time = 2;
-        public const int Full_Time = 1;
-        public static int computeEmpWage(string company, int empRatePerHr,int numOfWorkDay,int MaxWorkHr)
-        {
-            int empHr = 0;
-            int totalwage = 0;
-            int totalWorkDays = 0;
-            int totalEmpHrs = 0;
-
-            while(totalEmpHrs <= MaxWorkHr && totalWorkDays < numOfWorkDay)
-            {
-                totalWorkDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-
-                switch (empCheck)
-                {
-                    case Full_Time:
-                        empHr = 8;
-                        break;
-                    case Part_Time:
-                        empHr = 4;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-                }
-                totalEmpHrs += empHr;
-            }
-            totalwage = totalEmpHrs * empRatePerHr;
-            Console.WriteLine("Day: " + totalWorkDays + "\nEmployee working Hrs: " + totalEmpHrs);
-            Console.WriteLine("Total Wage for company: " +company  + " is: " + totalwage);
-            return totalwage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Jio",20,2,10);
-            computeEmpWage("Airtel",15,2,12);
+            EmpWageBuilder Jio = new EmpWageBuilder("Jio", 20, 2, 10);
+            EmpWageBuilder Airtel = new EmpWageBuilder("Airtel", 15, 2, 12);
+            Jio.computeEmpWage();
+            Console.WriteLine(Jio.ToString());
+            Airtel.computeEmpWage();
+            Console.WriteLine(Airtel.ToString());
         }
     }
 }
